14ee8d041073cbbef34201e685fdc6c7c8281370
Pramod Kharade <khara@pc-pramodk.secure.rrootshell.com>
2022-06-06 21:11:44 +0530

    ORM type

---
+++ src/lesson/lesson.type.ts
@@ -0,0 +1,17 @@
+import { Field, ID, ObjectType } from '@nestjs/graphql';
+
+@ObjectType('Lesson')
+export class LessonType {
+ 
+    @Field(type=>ID)
+  id: string;
+
+  @Field()
+  name: string;
+
+  @Field()
+  startDate: string;
+
+  @Field()
+  endStarte: string;
+}
